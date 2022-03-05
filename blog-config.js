555dbbc139b663b65c324749cb25238012c14964
riyenas0925 <riyenas0925@gmail.com>
2022-03-05 18:13:37 +0900

    doc: add resume notion link

--- blog-config.js
+++ blog-config.js
@@ -6,6 +6,7 @@ module.exports = {
   links: {
     github: "https://github.com/riyenas0925",
     email: "mailto:devhudi@gmail.com",
+    etc: "https://www.notion.so/riyenas0925/94ea44c1e7f04bc48d03766b0017a606"
   },
   utterances: {
     repo: "riyenas0925/mini.log",
